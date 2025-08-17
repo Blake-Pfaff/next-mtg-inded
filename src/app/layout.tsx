dfa8a481b2fbd6e02188ac71f36e90012ca66210
Blake Pfaff <blake.a.pfaff2@gmail.com>
2025-08-17 11:05:16 -0500

    Update layout.tsx to use LayoutWrapper for consistent header and footer application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { LayoutWrapper } from "../components/Layout";
 import "./globals.css";
 import { LayoutWrapper } from "../components/Layout";
 
@@ -28,6 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning={true}
       >
         <LayoutWrapper>{children}</LayoutWrapper>
       </body>
