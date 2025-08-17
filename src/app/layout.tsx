905440fafb7d25f1ca8e2c682b190cc54974f4ea
Blake Pfaff <blake.a.pfaff2@gmail.com>
2025-08-17 11:11:40 -0500

    remove dup import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { LayoutWrapper } from "../components/Layout";
 import "./globals.css";
-import { LayoutWrapper } from "../components/Layout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
